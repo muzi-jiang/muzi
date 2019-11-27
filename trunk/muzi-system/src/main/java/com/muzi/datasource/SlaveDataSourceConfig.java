1517763c012f2647d87c413a09f3716b55f586cd
muzi <1421686777@qq.com>
2019-11-27 23:45:45 +0800

    11/27

---
+++ trunk/muzi-system/src/main/java/com/muzi/datasource/SlaveDataSourceConfig.java
@@ -0,0 +1,69 @@
+package com.muzi.datasource;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+// 扫描 Mapper 接口并容器管理
+@MapperScan(basePackages = SlaveDataSourceConfig.PACKAGE, sqlSessionFactoryRef = "slaveSqlSessionFactory")
+public class SlaveDataSourceConfig {
+
+    // 精确到 slave 目录，以便跟其他数据源隔离
+    static final String PACKAGE = "com.muzi.test.webmagic.mapper";
+    static final String MAPPER_LOCATION = "classpath:mybatis/mapper/webmagic/*.xml";
+
+    @Value("${slave.datasource.url}")
+    private String url;
+
+    @Value("${slave.datasource.username}")
+    private String user;
+
+    @Value("${slave.datasource.password}")
+    private String password;
+
+    @Value("${slave.datasource.driverClassName}")
+    private String driverClass;
+
+    @Bean(name = "slaveDataSource")
+    public DataSource slaveDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean(name = "slaveTransactionManager")
+    public DataSourceTransactionManager slaveTransactionManager() {
+        return new DataSourceTransactionManager(slaveDataSource());
+    }
+
+    @Bean(name = "slaveSqlSessionFactory")
+    public SqlSessionFactory slaveSqlSessionFactory(@Qualifier("slaveDataSource") DataSource slaveDataSource)
+            throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(slaveDataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources(SlaveDataSourceConfig.MAPPER_LOCATION));
+
+        /*配置数据库和实体类大小写的区分*/
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session
+                .Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sessionFactory.setConfiguration(configuration);
+
+        return sessionFactory.getObject();
+    }
+}
