fcc8791a50c16d6160b84cc5795912d92595197f
libin <1421686777@qq.com>
2020-12-16 15:39:26 +0800

    flowable

---
+++ trunk/muzi_parent/muzi_flowable/src/main/java/com/muzi/flowable/test/constant/Constant.java
@@ -0,0 +1,10 @@
+package com.muzi.flowable.test.constant;
+
+/**
+ * @Author: libin
+ * @Date: 2020/12/16 15:37
+ */
+public class Constant {
+
+    public static final String LEAVE_KEY = "LEAVE";
+}
